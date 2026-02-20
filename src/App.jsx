557b9078a7609a4cb0f8ce78b5471286bd38c55d
veronique <owonazida.com>
2026-02-20 10:49:19 +0100

    Fix imports and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/NavBar";
+import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,7 +10,7 @@ import NotFound from "./pages/not_found";
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Project />} />
