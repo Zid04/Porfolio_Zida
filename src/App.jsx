473daa1f7b21c0d72b433ce86c42046e235240f1
veronique <owonazida.com>
2026-02-20 11:25:22 +0100

    modification de l'aspect tablette et telephone

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,30 @@ import About from "./pages/About";
 import Project from "./pages/Project";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/not_found";
+import MobileScroll from "./pages/MobileScroll";
 
 function App() {
+  // Mobile + tablette : largeur < 1024px
+  const isMobile = window.innerWidth < 1024;
+
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Project" element={<Project />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+
+      {isMobile ? (
+        // 👉 Sur mobile : scroll continu
+        <MobileScroll />
+      ) : (
+        // 👉 Sur ordinateur : navigation classique
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Project" element={<Project />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
+
       <Footer />
     </Router>
   );
